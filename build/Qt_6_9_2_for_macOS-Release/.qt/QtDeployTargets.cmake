fdf3478a23895b74691de74d8e2fa5d8f3243c60
AlexanderThaep <odomsathshia_tep@brown.edu>
2025-12-09 19:23:18 -0500

    Remove build subdirectories

--- build/Qt_6_9_2_for_macOS-Release/.qt/QtDeployTargets.cmake
+++
@@ -1,4 +0,0 @@
-set(__QT_DEPLOY_TARGET_astroview_FILE /Users/o_alex_t/Documents/Brown/CS1230/cs1230-astroview/build/Qt_6_9_2_for_macOS-Release/astroview)
-set(__QT_DEPLOY_TARGET_astroview_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_StaticGLEW_FILE /Users/o_alex_t/Documents/Brown/CS1230/cs1230-astroview/build/Qt_6_9_2_for_macOS-Release/libStaticGLEW.a)
-set(__QT_DEPLOY_TARGET_StaticGLEW_TYPE STATIC_LIBRARY)
